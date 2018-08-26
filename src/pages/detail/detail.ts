e4908a3e00a4ef5b2ce5f199206380e87db65196
kelechizy <kelechi.charles@gmail.com>
2018-08-26 08:33:58 -0700

    Update detail page with iframe video

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-detail',
   templateUrl: 'detail.html'
 })
 export class DetailPage {
-  constructor(public navCtrl: NavController) {
+  url: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public sanitizer: DomSanitizer) {
+    var item = navParams.get('item');
+
+    var playlistUrl = 'https://www.youtube.com/embed/videoseries?list=' + item.playlistId;
+
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(playlistUrl);
   }
 }
